cd67d2013b993d3f10766b4330761c5cdab77af5
HyxiaoGe <18889592303@163.com>
2024-12-03 22:58:44 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Textarea } from "@/components/Textarea";
 
 export default function Home() {
   return (
-    <div>
-      Root Page <Button variant="ghost" type="submit">Click me</Button>
+    <div className="flex items-center justify-center h-screen">
+      <form className="w-full max-w-md flex flex-col gap-4">
+        <h1 className="text-center text-2xl font-bold">Create App</h1>
+        <Input name="name" type="text" placeholder="Enter your name" />
+        <Textarea name="message" placeholder="Enter your message" />
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 }
